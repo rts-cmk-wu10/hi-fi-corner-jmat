a888097d8a31875a65c8a482fae9048134211dba
jlnbjrggrdndrsn <julianandersen9@gmail.com>
2023-09-22 11:30:03 +0200

    slider done!

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,10 +1,48 @@
-const NEXT_IMG = document.querySelector("#Next")
-const PREV_IMG = document.querySelector("#Prev")
+let sliderImages = document.querySelectorAll('.slide'),
+  arrowLeft = document.querySelector('#arrow-left'),
+  arrowRight = document.querySelector('#arrow-right'),
+  current = 0;
 
-NEXT_IMG.addEventListener("click",(function(){
-	document.querySelector("#Slider").append(document.querySelector("#Slider img:first-of-type"));
-}));
+// clear all images
+function reset(){
+  for(let i = 0; i < sliderImages.length; i++){
+    sliderImages[i].style.display = 'none';
+  }
+}
+ // initialise slider
+function startSlide(){
+  reset();
+  sliderImages[0].style.display = 'block';
+}
 
-PREV_IMG.addEventListener("click", (function(){
-	document.querySelector("#Slider").prepend(document.querySelector("#Slider img:last-of-type"));
-}));
+// show previous
+function slideLeft(){
+  reset();
+  sliderImages[current - 1].style.display = 'block';
+  current--;
+}
+
+// show next
+function slideRight(){
+  reset();
+  sliderImages[current + 1].style.display = 'block';
+  current++;
+}
+
+// left arrow click
+arrowLeft.addEventListener('click', function(){
+  if (current === 0){
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+
+// right arrow click
+arrowRight.addEventListener('click', function(){
+  if (current === sliderImages.length - 1){
+    current = -1;
+  }
+  slideRight();
+});
+
+startSlide();
