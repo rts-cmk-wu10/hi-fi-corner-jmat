46699b61e151eef8e946c3e7c626c828bdb495c1
ABertie <U2439307@rts-365.dk>
2023-09-28 14:05:12 +0200

    singel product style

--- assets/javaScript/produkt.js
+++ assets/javaScript/produkt.js
@@ -13,7 +13,7 @@ fetch("http://localhost:3000/produkt")
     })
     .then(function(data) {
         data.forEach(function(produkt) {
-            if (produkt.id.toString() === URL.get("produkt")) 
+            if (produkt.id.toString() === URL.get("produkt")) {
                 console.log(produkt);
 
                 IMG.src = produkt.image_src
@@ -22,7 +22,7 @@ fetch("http://localhost:3000/produkt")
                 BOX.innerHTML = 
                     `<h1 class="productDetails__name">${produkt.name.charAt(0).toUpperCase() + produkt.name.slice(1)}</h1>
                     <a href="/category.html?brand=${produkt.brand.name}" class="productDetails__brandLink">See other ${produkt.brand.name.charAt(0).toUpperCase() + produkt.brand.name.slice(1)} products</a>
-                    ${produkt.price.discaont !== null ? `<p class="productDetails__price"><span class="productDetails__price--normal">${produkt.price.normal_price}</span>${produkt.price.discaont}</p>` : `<p class="productDetails__price">${produkt.price.normal_price}</p>`}
+                    ${produkt.price.discaont !== "" ? `<p class="productDetails__price"><span class="productDetails__price--normal">£${produkt.price.normal_price}</span>£${produkt.price.discaont}</p>` : `<p class="productDetails__price">£${produkt.price.normal_price}</p>`}
                     <p class="productDetails__text">${produkt.description.text}</p>`
             
                 ADDITINAL_TABEL.innerHTML = 
@@ -56,54 +56,55 @@ fetch("http://localhost:3000/produkt")
                     DESCRIPTION_TABEL.innerHTML = 
                     `<tbody>
                         <tr>
-                            <td>Power Output (8/4 Ohm RMS)</td>
+                            <th>Power Output (8/4 Ohm RMS)</th>
                             <td>${produkt.description.power_output.toString().replace(",", " / ").replace(",", " / ")}</td>
                         </tr>
                         <tr>
-                            <td>Frequency response</td>
+                            <th>Frequency response</th>
                             <td>${produkt.description.frequency_response.toString().replace("", "X").replace(",", " / ")}</td>
                         </tr>
                         <tr>
-                            <td>Total Harmonic Distortion</td>
+                            <th>Total Harmonic Distortion</th>
                             <td>${produkt.description.total_harmonic_distortion.toString().replace(",", " / ")}</td>
                         </tr>
                         <tr>
-                            <td>Damping Factor</td>
+                            <th>Damping Factor</th>
                             <td>${produkt.description.damping_factor.toString().replace(",", " / ")}</td>
                         </tr>
                         <tr>
-                            <td>Input sensitivity:MM</td>
+                            <th>Input sensitivity:MM</th>
                             <td>${produkt.description["input_sensitivity:MM"].toString().replace(",", " / ")}</td>
                         </tr>
                         <tr>
-                            <td>Input sensitivity:MC</td>
+                            <th>Input sensitivity:MC</th>
                             <td>${produkt.description["input_sensitivity:MC"].replace("", "X")}</td>
                         </tr>
                         <tr>
-                            <td>Signal to Noise Ratio:MM / MC</td>
+                            <th>Signal to Noise Ratio:MM / MC</th>
                             <td>${produkt.description["signal_to_noise_ratio:MM/MC"].toString().replace(",", " / ") + produkt.description["signal_to_noise_ratio:MM/MC"][1].replace("", "X")}</td>
                         </tr>
                         <tr>
-                            <td>Input Sensitivity: High level</td>
+                            <th>Input Sensitivity: High level</th>
                             <td>${produkt.description["input_sensitivity:High_level"].toString().replace(",", " / ")}</td>
                         </tr>
                         <tr>
-                            <td>Input Sensitivity: Balanced High level</td>
+                            <th>Input Sensitivity: Balanced High level</th>
                             <td>${produkt.description["input_sensitivity:balanced_high_level"].replace("", "X")}</td>
                         </tr>
                         <tr>
-                            <td>Signal to Noise Ratio: High level</td>
+                            <th>Signal to Noise Ratio: High level</th>
                             <td>${produkt.description["signal_to_noise_ratio:high_level"].toString().replace(",", " / ")}</td>
                         </tr>
                         <tr>
-                            <td>Input Sensitivity: Power Amp Direct IN</td>
+                            <th>Input Sensitivity: Power Amp Direct IN</th>
                             <td>${produkt.description["input_sensitivity:power_amp_direct_IN"].replace("", "X")}</td>
                         </tr>
                         <tr>
-                            <td>Signal to Noise ratio :Power AMP Direct IN</td>
+                            <th>Signal to Noise ratio :Power AMP Direct IN</th>
                             <td>${produkt.description["signal_to_noise_ratio:power_amp_direct_IN"].replace("", "X")}</td>
                         </tr>
                     </tbody>`
+            }
         })
     })
     .catch(function (error) {
